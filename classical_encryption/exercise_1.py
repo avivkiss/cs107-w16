e1911c1bdfb59b83854c31c95f62c276d6323789
Aviv Kiss <avivkiss@gmail.com>
2016-01-12 16:50:36 -0800

    Update exercise_1.py

--- classical_encryption/exercise_1.py
+++ classical_encryption/exercise_1.py
@@ -1,10 +1,10 @@
 import random, itertools
 from collections import defaultdict
 """
-Let Z_9 = {0, 1, ...8}. Consider the symmetric encryption scheme in which
+Let Z_8 = {0, 1, ...7}. Consider the symmetric encryption scheme in which
 a message m = m[0]m[1]m[2]m[3] is a four-digit string containing only numbers 
-0-8, a permutation of Z_9 is randomly selected as a key pi, and the ciphertext
-c = c[0]c[1]c[2]c[3] = E(pi, m), also a four digit string from digits 0-8 is
+0-8, a permutation of Z_8 is randomly selected as a key pi, and the ciphertext
+c = c[0]c[1]c[2]c[3] = E(pi, m), also a four digit string from digits 0-7 is
 computed as follows:
 """
 
